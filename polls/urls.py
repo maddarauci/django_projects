c6d68a473bbfa2d570a110e65d7b323488b8ace4
maddarauci <dominicd165@gmail.com>
2022-05-26 01:21:08 +1000

    modified:   polls/templates/polls/detail.html 	modified:   polls/urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -5,6 +5,7 @@ from django.urls import URLPattern, path
 
 from . import views 
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
