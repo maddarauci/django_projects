a4c56aa7f8d54d2948b7f9425d9b879471a241c5
maddarauci <dominicd165@gmail.com>
2022-05-25 23:59:42 +1000

    modified:   polls/urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,14 @@
 # polls/urls.py 
 
 from nturl2path import url2pathname
+from termios import VDISCARD
 from django.urls import URLPattern, path 
 
 from . import views 
 
 urlpatterns = [
     path('', views.index, name='index')
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
