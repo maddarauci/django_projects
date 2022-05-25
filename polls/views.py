94e122f162490762a12731901d0c3447cab2836c
maddarauci <dominicd165@gmail.com>
2022-05-26 01:35:11 +1000

    new file:   polls/templates/polls/results.html 	modified:   polls/views.py

--- polls/views.py
+++ polls/views.py
@@ -37,6 +37,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
     response = "You are looking at the results of question %s."
     return HttpResponse(response % question_id)
 
