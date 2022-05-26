d1f8abb33c3615ccc6f63328d91cc5bb6b8f1ead
maddarauci <dominicd165@gmail.com>
2022-05-27 00:18:29 +1000

    modified:   polls/tests.py 	modified:   polls/views.py

--- polls/views.py
+++ polls/views.py
@@ -8,8 +8,40 @@ from django.views import generic
 from .models import Choice, Question
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+
+'''
 def IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'lastest_question_list'
@@ -22,18 +54,19 @@ def IndexView(generic.ListView):
     #lastest_question_list = Question.objects.order_by('-pub_date')[:5]
     #context = {'lastest_question_list': lastest_question_list}
     #return render(request, 'polls/index.html', context)
-    '''
+    
     template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': lastest_question_list,
-    }'''
+    }
     #return HttpResponse(template.render(context, request))
     #output = ', '.join([q.question_text for q in lastest_question_list])
     #return HttpResponse(output)
     #eturn HttpResponse("Hello, world. You're at the polls index.")
+'''
 
 
-
+'''
 # might raise 404 error.
 def DetailView(generic.DetailView):
     model = Question
@@ -74,3 +107,4 @@ def vote(request, question_id):
         # user hits the back btn.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
     #return HttpResponse("You are voting on question %s." % question_id)
+'''
